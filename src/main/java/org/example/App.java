b8e021715cba3f1fe5cd1b00f48f52d468acb09e
reddy-vivek1 <eegavivekreddy@gmail.com>
2024-02-28 18:37:10 -0500

    start

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,16 +15,16 @@ public class App
         Scanner sc =new Scanner(System.in);
         num=sc.nextInt();
         int i;
-        for(i=0; i<num; i++){
+        for(i=1; i<=num; i++){
         fizzBuzz(i);
         }
-        //System.out.println( "Hello World!" );
+
     }
     public static void fizzBuzz(int i){
         if(i==3){
             System.out.print("Fizz" + " ");
         }
-        if(i==5){
+        else if (i==5) {
             System.out.print("Buzz"+ " ");
         }
         else System.out.print(i + " ");
